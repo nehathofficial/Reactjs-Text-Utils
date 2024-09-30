1c7ed3db5522d3eb0632fc4eadc92a4127977b3c
Neha Thakur <nehathakur28official@gmail.com>
2024-09-30 14:19:13 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-sm navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.logoTitle}</a>
+        <a className="navbar-brand" href="#">{props.logoTitle}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="mynavbar">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
+              <a className="nav-link" href="#">Home</a>
             </li>
             {/* <li className="nav-item">
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
